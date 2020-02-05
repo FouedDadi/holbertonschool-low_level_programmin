0472454d0973af815e51a368bf07b67a901f324a
FouedDadi <foued.dadii@gmail.com>
2020-02-05 13:57:27 +0000

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,13 +5,9 @@
 *Return:0
 */
 int main(void)
-{int intType;
-float floatType;
-double doubleType;
-char charType;
-printf("Size of int: %ld bytes\n", sizeof(intType));
-printf("Size of float: %ld bytes\n", sizeof(floatType));
-printf("Size of double: %ld bytes\n", sizeof(doubleType));
-printf("Size of char: %ld byte\n", sizeof(charType));
+{printf("Size of int: %ld bytes\n", sizeof(int));
+printf("Size of float: %ld bytes\n", sizeof(float));
+printf("Size of double: %ld bytes\n", sizeof(double));
+printf("Size of char: %ld byte\n", sizeof(char));
 return (0);
 }
