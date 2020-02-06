df66c0292f2a8cdd36b3aad5e0c5247adda9253e
FouedDadi <foued.dadii@gmail.com>
2020-02-06 11:35:38 +0000

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
- * int x will receive A;
- * fucntion main will print the alphabet in lowercase;
- * return (0);
+ *main - fucntion main will print the alphabet in lowercase
+ *Return: 0
  */
 int main(void)
 {
