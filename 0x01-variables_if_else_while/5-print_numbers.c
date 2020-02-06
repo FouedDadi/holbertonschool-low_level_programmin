7a35cc3234fd880c2c6ecaf78646d47b65511527
FouedDadi <foued.dadii@gmail.com>
2020-02-06 13:54:39 +0000

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *execpt q & e
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int x;
+  x = 0;
+  while (x <= '9')
+ {
+   printf ("%d", x);
+   x++;
+}
+  printf("\n");
+  return (0);
+}
+
