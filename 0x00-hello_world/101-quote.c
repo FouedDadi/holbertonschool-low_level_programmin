49a808079470e4b6fb47dcb3a45f0a32e7f81308
FouedDadi <foued.dadii@gmail.com>
2020-02-06 16:09:11 +0000

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
-#include<stderr.h>
-#include<stdout.h>
+#include<unistd.h>
+#include<stdio.h>
 /**
  *main will print an error
  *use write to print a message as an error
@@ -7,6 +7,6 @@
 */
 int main(void)
 {
-write(2,"\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
