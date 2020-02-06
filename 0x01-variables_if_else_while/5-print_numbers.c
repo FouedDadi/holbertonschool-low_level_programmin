89563b7f1660495d414a57308f49e485dac82d24
FouedDadi <foued.dadii@gmail.com>
2020-02-06 14:23:38 +0000

    5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-  int x;
-  x = 0;
-  while (x <= '9')
- {
-   printf ("%d", x);
-   x++;
+int x;
+x = 0;
+while (x <= '9')
+{
+printf("%d", x);
+x++;
 }
-  printf("\n");
-  return (0);
+printf("\n");
+return (0);
 }
 
