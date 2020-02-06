32d817eacbf3920dedcb280ebe008509362a0f37
FouedDadi <foued.dadii@gmail.com>
2020-02-06 13:28:33 +0000

    msg

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - fucntion main will print the alphabet in lowercase
+ *Return: 0
+ */
+int main(void)
+{ char x;
+x = 'a';
+while (x <= 'z')
+{
+if (x == 'e' || x == 'q')
+{
+x+1;
+}
+putchar (x);
+x++;
+}
+putchar ('\n');
+return (0);
+}
