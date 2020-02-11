8fc3cba751932f305444474c80c7a13255409a1e
FouedDadi <foued.dadii@gmail.com>
2020-02-11 08:19:25 +0000

    1

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,14 +1,15 @@
 #include <unistd.h>
-int main(void)
+int main()
 {
 _putchar('H');
- _putchar('o');
- _putchar('l');
- _putchar('b');
- _putchar('e');
- _putchar('r');
- _putchar('t');
- _putchar('o');
- _putchar('n');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
 _putchar('\n');
+return 0;
 }
