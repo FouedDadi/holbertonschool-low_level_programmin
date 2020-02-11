320a64d1384359a52064b4fe330fa1a3a11978d4
FouedDadi <foued.dadii@gmail.com>
2020-02-11 14:44:34 +0000

    10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 /**
  *add- add two ints
- *@y:first integer
- *@x:second integer
+ *@y: first integer
+ *@x: second integer
  *Return: m
  */
-int add(int y, int x);
+int add(int y, int x)
 {
 int m;
 m = y + x;
