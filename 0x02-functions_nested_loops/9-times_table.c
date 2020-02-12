9d1e3981b18d9d98c483f252150a36d0231440dc
FouedDadi <foued.dadii@gmail.com>
2020-02-12 11:16:26 +0000

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,45 @@
 #include "holberton.h"
 /**
- *
- *
+ *times_table - show multiplication table
+ *Return: nothing
  */
 void times_table(void)
 {
-
+int x, y, S;
+for (x = 0; x <= 9; x++)
+{
+for (y = 0; y <= 9; y++)
+{
+S = x * y;
+if (S < 10)
+{
+_putchar((S) + '0');
+if (y < 9)
+{
+  if (((y + 1) * x) >= 10)
+{
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+}
+}
+else
+{
+_putchar((S / 10) + '0');
+_putchar((S % 10) + '0');
+if (y < 9)
+{
+_putchar (',');
+_putchar (' ');
+}
+}
+}
+_putchar ('\n');
+}
 }
