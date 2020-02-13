7115f88d426dcd6ec92f7cfeb01b800b8abf065f
FouedDadi <foued.dadii@gmail.com>
2020-02-13 22:49:18 +0000

    101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,15 +5,17 @@
  */
 void print_number(int n)
 {
-if (n < 0)
+if (n % 10 != 0)
 {
-_putchar('-');
-_putchar(n / 10 + '0');
+if (n > 0)
+{
 _putchar(n % 10 + '0');
-n = -n;
+else 
+_putchar(-n % 10 + '0')
 }
-if (n == 0)
-_putchar('0');
+}
+if ((n % 10 == 0) && (n > 0))
+_putchar(n % 10 + '0');
 if (n > 10)
 _putchar(n / 10 + '0');
 _putchar(n % 10 + '0');
