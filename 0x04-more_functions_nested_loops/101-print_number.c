76c9c5d5c452312e64cf7ede90dae87b67d7f010
FouedDadi <foued.dadii@gmail.com>
2020-02-13 22:35:04 +0000

    all

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *print_number- display a number
+ *
+ */
+void print_number(int n)
+{
+if (n < 0)
+{
+putchar('-');
+n = -n;
+}
+if (n == 0)
+putchar('0');
+if (n/10)
+printnumber(n/10);
+putchar(n%10 + '0');
+}
