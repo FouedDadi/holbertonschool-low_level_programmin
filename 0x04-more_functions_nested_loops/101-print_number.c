74cc17a5f680381d65e90bb98b2a4bf4a302e52e
FouedDadi <foued.dadii@gmail.com>
2020-02-14 09:11:08 +0000

    101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 /**
  *print_number- display a number
- *
+ *@n: integer
  */
 void print_number(int n)
 {
 if (n / 10 != 0)
 {
-_putchar(n / 10);
-if (n > 0 )
+print_number(n / 10);
+if (n > 0)
 {
 _putchar(n % 10 + '0');
 }
@@ -19,11 +19,13 @@ _putchar(-n % 10 + '0');
 }
 else if ((n / 10 == 0) && (n % 10 != 0) && (n > 0))
 {
-_putchar(n % 10 + '0');
+_putchar((n % 10) + '0');
 }
 else if ((n / 10 == 0) && (n % 10 != 0) && (n <= 0))
 {
 _putchar('-');
 _putchar(-i % 10 + '0');
 }
+else if (n == 0)
+_putchar('0')
 }
