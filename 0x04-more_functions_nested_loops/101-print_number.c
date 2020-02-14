fee98971bff0e15ba88f974dfa20ea7379d2224a
FouedDadi <foued.dadii@gmail.com>
2020-02-14 09:44:23 +0000

    101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -16,16 +16,16 @@ else
 {
 _putchar(-n % 10 + '0');
 }
-}
-else if ((n / 10 == 0) && (n % 10 != 0) && (n > 0))
-{
-_putchar((n % 10) + '0');
+else if (n == 0)
+_putchar('0')
 }
 else if ((n / 10 == 0) && (n % 10 != 0) && (n <= 0))
 {
 _putchar('-');
 _putchar(-n % 10 + '0');
 }
-else if (n == 0)
-_putchar('0');
+else if ((n / 10 == 0) && (n % 10 != 0) && (n > 0))
+{
+_putchar((n % 10) + '0');
+}
 }
