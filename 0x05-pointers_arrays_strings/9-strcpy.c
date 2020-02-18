e3ae00eb26c4cc4c5d21837e7a86e04070826ddf
FouedDadi <foued.dadii@gmail.com>
2020-02-18 17:19:34 +0000

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *_strcpy- copy a string
+ *@dest: destination
+ *@src: source
+ */
+char *_strcpy(char *dest, char *src)
+{
+char *x = dest;
+while(*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+} 
+*dest = '\0';
+return x;
+}
