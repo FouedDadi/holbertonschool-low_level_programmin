aff24de15516983edfba6e430cb06831e8104273
FouedDadi <foued.dadii@gmail.com>
2020-02-18 13:42:05 +0000

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+B#include "holberton.h"
+/**
+ *puts2- print values of even numbers
+ *@str: pointer
+ *
+ */
+void puts2(char *str)
+{
+int i, l = 0;
+while (str[l])
+l++;
+for (i = 0 ; i < l; i++)
+{
+if (i % 2 == 0)
+_putchar(str[i]);
+}
+_putchar('\n');
+}
