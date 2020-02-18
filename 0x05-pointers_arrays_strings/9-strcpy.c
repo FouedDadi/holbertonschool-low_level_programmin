3019cd56840f2944a3ca644a960a8101775acfe6
FouedDadi <foued.dadii@gmail.com>
2020-02-18 17:21:59 +0000

    9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,16 +3,17 @@
  *_strcpy- copy a string
  *@dest: destination
  *@src: source
+ *Return: return x
  */
 char *_strcpy(char *dest, char *src)
 {
 char *x = dest;
-while(*src != '\0')
+while (*src != '\0')
 {
 *dest = *src;
 dest++;
 src++;
-} 
+}
 *dest = '\0';
-return x;
+return (x);
 }
