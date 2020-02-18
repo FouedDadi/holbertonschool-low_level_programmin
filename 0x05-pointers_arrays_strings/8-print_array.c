84e4f365c87e621aa7a2c9dbefea050b088e4aa5
FouedDadi <foued.dadii@gmail.com>
2020-02-18 17:05:29 +0000

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *print_array- print an array n times
+ *@a: pointer
+ *@n: number of prints
+ */
+void print_array(int *a, int n)
+{
+int x;
+for (x = 0; x < n; x++)
+{
+if (x < n - 1)
+printf("%d, ", a[x]);
+else if (x >= n - 1)
+printf("%d", a[x]);
+}
+printf("\n");
+}
