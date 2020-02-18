76a94a591b1eb02672de78013008dcd1e9559046
FouedDadi <foued.dadii@gmail.com>
2020-02-18 08:41:37 +0000

    2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,12 @@
 /**
  *_strlen- lengh of string
  *@s: pointer s
- *@l: integer l
- *Return: return l 
+ *Return: return l
  */
 int _strlen(char *s)
 {
 int l = 0;
 while (*s++)
 l++;
-return l;
+return (l);
 }
