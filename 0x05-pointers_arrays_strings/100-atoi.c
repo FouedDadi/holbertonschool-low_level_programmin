c3b380a84139addfa8ccdbfa11758a4a43b36e42
FouedDadi <foued.dadii@gmail.com>
2020-02-19 11:57:34 +0000

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,7 +7,7 @@
 int _atoi(char *s)
 {
 int i = 0;
-int result =0;
+int result = 0;
 int sign = 1;
 while (s[i] != '\0')
 {
@@ -15,9 +15,9 @@ if (s[i] == '-')
 sign = sign * -1;
 else if ((s[i] >= '0') && (s[i] <= '9'))
 {
-result = (result * 10) + ((s[i]) -'0');
+result = (result * 10) + ((s[i]) - '0');
 }
 i++;
 }
-return (result * sign);
+return (result *sign);
 }
