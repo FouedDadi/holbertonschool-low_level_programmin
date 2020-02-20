09ed192667d555c6761366c1f65126c19b5926cb
FouedDadi <foued.dadii@gmail.com>
2020-02-20 19:01:04 +0000

    13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -6,14 +6,19 @@
  */
 char *rot13(char *s)
 {
-int i;
-for (i = 0; s[i] != '\0'; i++)
+int i = 0;
+while (s[i] != '\0')
 {
 if ((s[i] >= 'a' && s[i] <='m') || (s[i] >= 'A' && s[i] <= 'M'))
+{
 s[i] = s[i] + 13;
+}
 else
 while ((s[i] >= 'n' && s[i] <='z') || (s[i] >= 'N' && s[i] <= 'Z'))
+{
 s[i] = s[i] - 13;
+}
+i++;
 }
 return (s);
 }
