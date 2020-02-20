c3984424c7023cb6ce417c9749fbc14690a5d056
FouedDadi <foued.dadii@gmail.com>
2020-02-20 11:41:31 +0000

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,8 +9,9 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-size_t x;
-for (x = 0; x < n && src[x] != '\0'; x++)
+int x;
+for (x = 0; src[x] != '\0'; x++)
+if (x < n)
 dest[x] = src[x];
 for ( ; x < n; x++)
 dest[x] = '\0';
