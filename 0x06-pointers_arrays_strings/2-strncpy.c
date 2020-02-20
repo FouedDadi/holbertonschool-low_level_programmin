528028b310639d7e5bbfa24b5712f5ffc9715384
FouedDadi <foued.dadii@gmail.com>
2020-02-20 11:21:39 +0000

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *strncpy- copy a string
+ *@dest: first string
+ *@src: second string
+ *@n: max n of bytes
+ *Return: return statement
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+char *p;
+p = dest;
+while (*src && n--)
+{
+*dest = *src;
+dest++;
+src++;
+}
+dest = '\0';
+return p;
+}
