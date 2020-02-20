ca4902618baa5b9ef429cd2c05fc89ccfe3cd019
FouedDadi <foued.dadii@gmail.com>
2020-02-20 11:38:28 +0000

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,18 +4,15 @@
  *@dest: first string
  *@src: second string
  *@n: max n of bytes
+ *@x: loop
  *Return: return statement
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-char *p;
-p = dest;
-while (*src && n--)
-{
-*dest = *src;
-dest++;
-src++;
-}
-dest = '\0';
-return (p);
+size_t x;
+for (x = 0; x < n && src[x] != '\0'; x++)
+dest[x] = src[x];
+for ( ; x < n; x++)
+dest[x] = '\0';
+return dest;
 }
