0b33e597cf1fe803b3d980ae34415cf49efc7e33
FouedDadi <foued.dadii@gmail.com>
2020-02-20 18:55:15 +0000

    13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *rot13- encode using rot13
+ *@s: pointer
+ *Return: return s
+ */
+char *rot13(char *s)
+{
+int i = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+if ((s[i] >= 'a' && s[i] <='m') || (s[i] >= 'A' && s[i] <= 'M'))
+s[i] = s[i] + 13;
+else
+while ((s[i] >= 'n' && s[i] <='z') || (s[i] >= 'N' && s[i] <= 'Z'))
+s[i] = s[i] - 13;
+}
+return (s);
+}
