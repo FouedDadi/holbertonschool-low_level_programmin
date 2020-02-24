3376d6b2974972340680d9f45e57d1af97a73259
FouedDadi <foued.dadii@gmail.com>
2020-02-24 17:14:37 +0000

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,9 +8,9 @@
 unsigned int _strspn(char *s, char *accept)
 {
 unsigned int x, y;
-for (x = 0; s[x]; x++)
+for (x = 0; s[x] != '\0'; x++)
 {
-for (y = 0; accept[y]; y++)
+for (y = 0; accept[y] != '\0'; y++)
 {
 if (s[x] == accept[y])
 break;
