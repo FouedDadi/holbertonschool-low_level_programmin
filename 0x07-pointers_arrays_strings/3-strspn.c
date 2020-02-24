b997c27b932b31f4f9736282806756102cea035e
FouedDadi <foued.dadii@gmail.com>
2020-02-24 15:12:57 +0000

    3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *_strspn- gets length of substring
+ *@s: pointer
+ *@accept: pointer
+ *Return: return statement
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+int x, y, w;
+for (x = 0; s[x] != '\0'; x++)
+{
+for (y = 0; accept[y] != '\0'; y++)
+{
+if (s[x] == accept[y])
+{
+w++;
+break;
+}
+}
+if (s[x] != accept[y])
+break;
+}
+return (w);
+}
