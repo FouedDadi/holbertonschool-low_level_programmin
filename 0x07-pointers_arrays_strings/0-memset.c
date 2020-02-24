934d58533f0392f55956cc960497aa301487eb55
FouedDadi <foued.dadii@gmail.com>
2020-02-24 12:50:15 +0000

    1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,13 +4,14 @@
  *@n: number of bytes pointed by *s
  *@b: value of memory to be filled
  *@s: pointer
+ *Return: return the pointer
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int x;
 for (x = 0; x < n; x++)
 {
-*s = b;
+s = b;
 s++;
 }
 return (s);
