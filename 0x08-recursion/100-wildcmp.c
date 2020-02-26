aeef4d5bb31f5fd65bc5c897023a451675bc48f2
FouedDadi <foued.dadii@gmail.com>
2020-02-26 19:27:44 +0000

    100

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -13,7 +13,5 @@ if (*s1 == *s2 && *s1 != '\0' && *s2 != '\0')
 return (wildcmp(s1 + 1, s2 + 1));
 if (*s2 == '*')
 return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
-if (*(s1 + 1) != '\0' && *s2 == '\0')
-return (0);
 return (0);
 }
