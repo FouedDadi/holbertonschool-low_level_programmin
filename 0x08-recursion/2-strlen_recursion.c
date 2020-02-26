3938b7ba750821226a722938ceafe7286b46a9fb
FouedDadi <foued.dadii@gmail.com>
2020-02-26 13:15:13 +0000

    2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,11 +1,16 @@
 #include "holberton.h"
 /**
- *
- *
+ *_strlen_recursion- return the length of a string
+ *@s: pointer
  */
 int _strlen_recursion(char *s)
 {
-
-
-
+int l = 0;
+if (s[l] != '\0')
+{
+l++;
+return (l + _strlen_recursion(s + 1));
+}
+else
+return (0);
 }
