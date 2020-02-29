4078ec81d85f7c12b2351edc3e15e5e52ee42163
FouedDadi <foued.dadii@gmail.com>
2020-02-29 21:56:52 +0000

    myname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,14 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *main- print the name of the program
+ *main - print the name of the program
  *@argc: arguments length
  *@argv: each argument content
+ *Return: 0
  */
 int main(int argc, char *argv[])
 {
-int x;
-for (x = 0; x < argc; x++)
-printf("%s ", argv[x]);
-printf("\n");
+if (argc == 1)
+printf("%s \n", argv[0]);
 return (0);
 }
