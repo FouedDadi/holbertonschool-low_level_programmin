93df8f0f99df7058ec29312ee76cd6b571c7d1b0
FouedDadi <foued.dadii@gmail.com>
2020-03-01 02:19:12 +0000

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,16 +11,18 @@
 int main(int argc, char *argv[])
 {
 int x, y, S = 0;
-if (argc < 2)
-{
-printf("0\n");
-return (0);
-}
 for (y = 1; argv[y] != '\0'; y++)
+{
 if (!isdigit(*argv[y]))
 {
 printf("Error\n");
 return (1);
+}
+}
+if (argc < 2)
+{
+printf("0\n");
+return (0);
 }
 for (x = 1; x < argc; x++)
 S += atoi(argv[x]);
