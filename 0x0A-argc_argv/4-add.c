00a556cd30cadb6bfb75f8cd55a6ee32090cb6ed
FouedDadi <foued.dadii@gmail.com>
2020-03-01 01:41:03 +0000

    4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ *main- print number of arguments
+ *@argc: number of arguments
+ *@argv: each argument
+ *Return: return 1 if argv[x] is not a digit
+ */
+int main(int argc, char *argv[])
+{
+int x, y, S = 0;
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
+for (y = 1; argv[y] != '\0'; y++)
+{
+if (!isdigit(*argv[y]))
+{
+printf("Error\n");
+return (1);
+}
+}
+for (x = 1; x < argc; x++)
+S += atoi(argv[x]);
+printf("%d\n", S);
+return (0);
+}
