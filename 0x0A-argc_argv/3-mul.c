92b070b4e6c70e37203992c67660dc779c6dd6c3
FouedDadi <foued.dadii@gmail.com>
2020-03-01 00:36:47 +0000

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main- print number of arguments
+ *@argc: number of arguments
+ *@argv: each argument
+ *Return: return 1 if argc < 3
+ */
+int main(int argc, char *argv[])
+{
+int x, y, S = 0;
+if (argc == 3)
+{
+x = atoi(argv[1]);
+y = atoi(argv[2]);
+S = x * y;
+printf("%d\n", S);
+}
+else
+{
+printf("error\n");
+return (1);
+}
+return (0); 
+}
