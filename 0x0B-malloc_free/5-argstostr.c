20541a924d6dd7e2fefd555d603d30d770518ea5
FouedDadi <foued.dadii@gmail.com>
2020-03-04 17:01:57 +0000

    5

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -11,20 +11,36 @@ char *argstostr(int ac, char **av)
 {
 int x, y, w = 0, l = 0;
 char *S;
-for (x = 0; x < ac; x++)
+if (ac == 0)
+return (NULL);
+if (av == NULL)
+return (NULL);
+while (x < ac)
 {
-for (y = 0; av[x][y] != '\0'; y++)
+y = 0;
+while (av[x][y] != '\0')
+{
 l++;
+y++;
+}
+x++;
 }
 S = malloc(l);
-for (x = 0; x < ac; x++)
+if (S == NULL)
+return (NULL);
+x = 0;
+while (x < ac)
 {
-for (y = 0; av[x][y] != '\0'; y++)
+y = 0;
+while (av[x][y] != '\0')
 {
 S[w] = av[x][y];
 w++;
+y++;
 }
-S[w++] = '\n';
+S[w] = '\n';
+w++;
+x++;
 }
 S[w] = '\0';
 return (S);
