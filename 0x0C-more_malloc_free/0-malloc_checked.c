3b81e9099bf5a8b01fef57ae904fe6760b41743d
FouedDadi <foued.dadii@gmail.com>
2020-03-05 16:33:38 +0000

    0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,16 +2,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *
- *
- *
- *
+ **malloc_checked- allocate memory for b and check if the function fails
+ *@b: unsigned integer
  */
 void *malloc_checked(unsigned int b)
 {
-
-
-
-
-
+int *x;
+x = malloc(unsigned int b);
+if (w == NULL)
+exit(98);
 }
