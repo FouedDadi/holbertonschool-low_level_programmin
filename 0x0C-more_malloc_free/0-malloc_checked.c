9b55afc3e3ab103207ee8d6d1d708a32b191ddb7
FouedDadi <foued.dadii@gmail.com>
2020-03-05 16:40:06 +0000

    0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,11 +4,13 @@
 /**
  **malloc_checked- allocate memory for b and check if the function fails
  *@b: unsigned integer
+ *Return: return the pointer x
  */
 void *malloc_checked(unsigned int b)
 {
 int *x;
-x = malloc(unsigned int b);
-if (w == NULL)
+x = malloc(b);
+if (x == NULL)
 exit(98);
+return (x);
 }
