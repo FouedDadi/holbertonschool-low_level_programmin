44f817365b2e3ede8d4e673955ffae52929d9fda
FouedDadi <foued.dadii@gmail.com>
2020-03-05 19:32:51 +0000

    3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,14 +9,15 @@
  */
 int *array_range(int min, int max)
 {
-int x;
+int x, y;
 int *p;
+y = max - min + 1;
 if (min > max)
 return (NULL);
-p = malloc(sizeof(int) * (max - min + 1));
+p = malloc(sizeof(int) * y);
 if (p == NULL)
 return (NULL);
-for (x = 0; x < max; x++)
+for (x = 0; x < y; x++)
 {
 p[x] = min;
 min++;
