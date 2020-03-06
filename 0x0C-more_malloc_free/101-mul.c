b365f207a0b5621cbad54df3030937e500521092
FouedDadi <foued.dadii@gmail.com>
2020-03-06 21:50:25 +0000

    101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,19 +9,13 @@
  */
 int main(int argc, char *argv[])
 {
-int mul, num1, num2, x, y;
+int mul, num1, num2;
 if (argc == 3)  
-{
-for (x = 0; x < argc; x++)
-{
-for (y = 0; argv[x][y] != '\0'; y++)
 {
 if (argv[1] >= 0 && argv[2] <= 9)
 num1 = atoi(argv[1]);
 if (argv[2] >= 0 && argv[2] <= 9)
 num2 = atoi(argv[2]);
-}
-}
 mul = num1 * num2;
 printf("%d\n", mul);
 if (argc != 3)
