5d07eab36aef6988f05b6ccbb84d4ac6d8f0dedd
FouedDadi <foued.dadii@gmail.com>
2020-03-06 21:53:59 +0000

    101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -6,24 +6,25 @@
  *main- multiply two positive numbers
  *@argc: number of positive numbers
  *@argv: each positive number
+ *Return: return 0
  */
 int main(int argc, char *argv[])
 {
 int mul, num1, num2;
-if (argc == 3)  
+if (argc == 3)
 {
 if (argv[1] >= 0 && argv[2] <= 9)
 num1 = atoi(argv[1]);
-if (argv[2] >= 0 && argv[2] <= 9)
+if (argv[2] >= '0' && argv[2] <= '9')
 num2 = atoi(argv[2]);
-mul = num1 * num2;
+mul = num1 *num2;
 printf("%d\n", mul);
 if (argc != 3)
 {
 printf("Error\n");
 exit(98);
 }
-if (isdigit(num1) == 0) && (isdigit(num2) == 0)
+if isdigit(num1) == 0 && isdigit(num2) == 0
 {
 printf("Error\n");
 exit(98);
