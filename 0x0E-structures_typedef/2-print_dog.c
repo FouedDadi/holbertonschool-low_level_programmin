d5f8eca608f5140d4a84ca59a587c679ccbfea80
FouedDadi <foued.dadii@gmail.com>
2020-03-09 20:38:09 +0000

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,12 +13,12 @@ if (d->name == NULL)
 printf("Name: (nil)");
 else
 printf("Name: %s\n", d->name);
-if (d->owner == NULL)
-printf("Owner: (nil)");
-else
-printf("Owner: %s\n", d->owner);
 if (d->age == 0)
 printf("Age: (nil)");
 else
 printf("Age: %f\n", d->age);
+if (d->owner == NULL)
+printf("Owner: (nil)");
+else
+printf("Owner: %s\n", d->owner);
 }
