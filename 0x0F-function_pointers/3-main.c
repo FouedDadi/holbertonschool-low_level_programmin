5f7cea6b0e8278b37734ac0103f12d4230c5be59
vagrant <vagrant@homestead.test>
2020-03-11 23:29:15 +0000

    3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main- main code
+ *@argc: number of arguments
+ *@argv: each argument
+ *Return: return 0
+ */
+int main (int argc, char *argv[])
+{
+int sum1, sum2;
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+if (get_op_func(argv[2]) == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+return (0);
+}
