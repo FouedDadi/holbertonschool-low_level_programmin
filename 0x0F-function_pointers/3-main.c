a92e85a166627aecd32e93817e95365a6a51501a
FouedDadi <60392233+FouedDadi@users.noreply.github.com>
2020-03-11 23:24:43 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,7 +9,7 @@
  */
 int main (int argc, char *argv[])
 {
-int num1, num2, res;
+int num1, num2, r;
 num1 = atoi(argv[1]);
 num2 = atoi(argv[3]);
 if (argc != 4)
@@ -22,7 +22,7 @@ if (get_op_func(argv[2]) == NULL)
 printf("Error\n");
 exit(99);
 }
-res = get_op_func((argv[2])(num1, num2);
+r = get_op_func((argv[2])(num1, num2);
 printf("%d\n", res);
 return (0);
 }
