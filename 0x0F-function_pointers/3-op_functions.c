c2eba8f4d1d9558f94c74ac243ccf0112f48689d
vagrant <vagrant@homestead.test>
2020-03-11 20:30:30 +0000

    op

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,73 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *op_add- fucntion that returns result of sum
+ *@a: integer
+ *@b: integer
+ *Return: return S
+ */
+int op_add(int a, int b)
+{
+int S;
+S = a + b;
+return (S);
+}
+/**
+ *op_sub- function returns difference btw a and b
+ *@a: integer
+ *@b: integer
+ *Return: return D
+ */
+int op_sub(int a, int b)
+{
+int D;
+D = a - b;
+return (D);
+}
+/**
+ *op_mul- function that multiply a and b
+ *@a: integer
+ *@b: integer
+ *Return: return M
+ */
+int op_mul(int a, int b)
+{
+int M;
+M = a * b;
+return (M);
+}
+/**
+ *op_div- function that return division of a/b
+ *@a: integer
+ *@b: integer
+ *Return: return d
+ */
+int op_div(int a, int b)
+{
+int d;
+d = a / b;
+if (b == 0)
+{
+printf("Error");
+exit(100);
+}
+return (d);
+}
+/**
+ *op_mod- function that return modulo of a % b
+ *@a: integer
+ *@b: integer
+ *Return: return m
+ */
+int op_mod(int a, int b)
+{
+int m;
+m = a % b;
+if (b == 0)
+{
+printf("Error");
+exit(100);
+}
+return (m);
+}
