d093be3d5fea3cca54f18ef039acea32ba48fa7e
vagrant <vagrant@homestead.test>
2020-03-11 17:39:14 +0000

    0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *print_name- function that prints a name
+ *@name: pointer to name
+ *@f: pointer to function that will take the name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
