f0f7f414351e1958ee2e01ad11947fb8a7b96d54
vagrant <vagrant@homestead.test>
2020-03-14 18:41:41 +0000

    all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ *sum_them_all- function that sums all args
+ *@n: number of arguments
+ *Return: return the result
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int S = 0, x;
+if (n == 0)
+return (0);
+va_list unintargp;
+va_start(unintargp, n);
+for (x = 0; x < n; x++)
+S += va_arg(unintargp, unsigned int);
+va_end(unintargp);
+return (S);
+}
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,16 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ *
+ *
+ *
+ *
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+
+
+}
