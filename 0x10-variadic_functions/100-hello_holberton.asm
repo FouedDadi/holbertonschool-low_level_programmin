b7a9cef42019b5b95cd10f57d46d0ce07878158e
vagrant <vagrant@homestead.test>
2020-03-14 22:34:08 +0000

    100

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,13 +1,29 @@
-	section     .text
-	global      _start
+	section .data
+	text db "Hello, Holberton",10,0
+	section .text
+	global _start
+
 _start:
-	mov     edx,len
-	mov     ecx,msg
-	mov     ebx,1
-	mov     eax,4
-	int     0x80
-	mov     eax,1
-	int     0x80
-	section     .data
-	msg     db  'Hello, Holberton',0xa
-	len     equ $ - msg
+	mov rax, text
+	call _print
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
+_print:
+	push rax
+	mov rbx, 0
+_printLoop:
+	inc rax
+	inc rbx
+	mov cl, [rax]
+	cmp cl, 0
+	jne _printLoop
+
+	mov rax, 1
+	mov rdi, 1
+	pop rsi
+	mov rdx, rbx
+	syscall
+
+	    ret
