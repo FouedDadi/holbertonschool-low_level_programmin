376a30cf88b9ff4630b23e7968b04acc02827901
vagrant <vagrant@homestead.test>
2020-03-14 18:50:22 +0000

    0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,10 +11,10 @@ int sum_them_all(const unsigned int n, ...)
 {
 int S = 0;
 unsigned int x;
-if (n == 0)
-return (0);
 va_list unintargp;
 va_start(unintargp, n);
+if (n == 0)
+return (S);
 for (x = 0; x < n; x++)
 S += va_arg(unintargp, unsigned int);
 va_end(unintargp);
