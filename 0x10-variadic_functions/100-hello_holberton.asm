3825b8d995565c743d2f5ca3076208a968c25836
vagrant <vagrant@homestead.test>
2020-03-14 22:54:46 +0000

    100

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,10 +1,10 @@
 	SECTION .data
-msg:	 db "Hello, Holberton", 15
+msg:	 db "Hello, Holberton", 10
 
 	SECTION .text
 	global main
 main:
-	mov edx, 16
+	mov edx, 17
 	mov ecx, msg
 	mov ebx, 1
 	mov eax, 4
