b5896da8aed9d99672ff4161268494784e9e0432
FouedDadi <foued.dadii@gmail.com>
2020-03-20 17:55:24 +0000

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,15 @@
  */
 size_t print_list(const list_t *h)
 {
-
-
-
+int x;
+for (x = 0; h != NULL; x++)
+{
+if (h->str != NULL)
+printf("[%d] %s", h->len, h->str);
+if (h->str == NULL)
+printf("[%d] (nil)", x);
+printf("\n");
+h = h->next;
+}
+return (x);
 }
