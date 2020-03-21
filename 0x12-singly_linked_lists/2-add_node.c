0caff60e8c4e95a55d2f04bc6609fe63c91845ed
FouedDadi <foued.dadii@gmail.com>
2020-03-21 23:58:32 +0000

    2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,13 +12,15 @@ list_t *add_node(list_t **head, const char *str)
 {
 unsigned int x;
 struct list_s *new_node = (struct list_s *) malloc(sizeof(struct list_s));
+if (new_node == NULL)
+return (NULL);
 if (new_node != NULL)
+{
 new_node->str = strdup(str);
 for (x = 0; str[x] != '\0'; x++)
 new_node->next = *head;
 new_node->len = x;
-if (new_node == NULL)
-return (NULL);
 *head = new_node;
+}
 return (new_node);
 }
