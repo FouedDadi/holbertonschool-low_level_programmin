80b9e460283743eb3d6b35eae8c1ebe859a61c2e
FouedDadi <foued.dadii@gmail.com>
2020-03-21 22:54:27 +0000

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,15 +2,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *
- *
- *
- *
+ *print_list- function to print all elements of list
+ *@h: pointer to head of list
+ *Return: return x
  */
 size_t print_list(const list_t *h)
 {
 int x;
-for (x = 0; h != NULL; x++)
+while (h != NULL)
 {
 if (h->str != NULL)
 printf("[%d] %s", h->len, h->str);
@@ -18,6 +17,7 @@ if (h->str == NULL)
 printf("[%d] (nil)", x);
 printf("\n");
 h = h->next;
+x++;
 }
 return (x);
 }
