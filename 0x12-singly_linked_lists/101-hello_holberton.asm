cb255a055015f59188386105165ff7f153dc2a07
FouedDadi <foued.dadii@gmail.com>
2020-03-22 19:09:05 +0000

    101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,19 @@
-	SECTION .data
-msg:		 db "Hello, Holberton", 10
+	section .text
+	global _start
 
-	SECTION .text
-	global main
-main:
-	mov edx, 17
-	mov ecx, msg
-	mov ebx, 1
-	mov eax, 4
+	section .data
+	msg db  'Hello, Holberton',0xa
+	len equ $ - msg
+
+	section .text
+_start:
+
+	mov edx,len
+	mov ecx,msg
+	mov ebx,1
+	mov eax,4
 	int 0x80
 
-	mov ebx, 0
-	mov eax, 1
-	int 0x80
+	mov ebx,0
+	mov eax,1
+	int 0x80    
