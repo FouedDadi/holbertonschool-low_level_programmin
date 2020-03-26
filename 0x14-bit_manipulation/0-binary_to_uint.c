7c44199c276adb039eed2035fa2ad888c20ddb7c
FouedDadi <foued.dadii@gmail.com>
2020-03-26 19:01:09 +0000

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,20 +1,32 @@
 #include "holberton.h"
 /**
+ *_strlen- lengh of string
+ *@s: pointer s
+ *Return: return l
+ */
+int _strlen(const char *s)
+{
+int l = 0;
+while (*s++)
+l++;
+return (l);
+}
+/**
  *binary_to_uint- function that converts binary to unsigned int
  *@b: pointer to binary number
- *Return: return en or 0
+ *Return: return val or 0
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int en = 0;
-int x = 0;
-if (b == NULL || (b[x] != '0' && b[x] != '1'))
-return (0);
-while (b[x] == '0' || b[x] == '1')
+unsigned int c = 1;
+unsigned int val = 0;
+int x;
+for (x = _strlen(b) - 1; x >= 0; x--)
 {
-en <<= 1;
-en += b[x] - '0';
-x++;
+if (b[x] == '1')
+val += c;
+c *= 2;
+if (b[x] != '0' && b[x] != '1')                                                                                       return (0);
 }
-return (en);
+return (val);
 }
