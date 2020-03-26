dee44b2e2fa8d00081af6a61d071c1d66b0d20c4
FouedDadi <foued.dadii@gmail.com>
2020-03-26 20:42:49 +0000

    2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,6 +1,8 @@
 #include "holberton.h"
 /**
  *get_bit- function that return value of a bit
+ *@n: number
+ *@index: index
  *Return: return x
  */
 int get_bit(unsigned long int n, unsigned int index)
@@ -8,4 +10,8 @@ int get_bit(unsigned long int n, unsigned int index)
 unsigned long int x;
 x = (n >> index) & 1;
 return (x);
+if (x != NULL)
+return (1);
+if (x == NULL)
+return (0);
 }
