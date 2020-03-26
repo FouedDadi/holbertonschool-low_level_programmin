541b461fb4ee45642116fb6565259d8336840b2f
FouedDadi <foued.dadii@gmail.com>
2020-03-26 18:39:42 +0000

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,9 +8,7 @@ unsigned int binary_to_uint(const char *b)
 {
 unsigned int en = 0;
 int x = 0;
-if (b == NULL)
-return (0);
-if (b[x] != '0' && b[x] != '1')
+if (b == NULL || (b[x] != '0' && b[x] != '1'))
 return (0);
 while (b[x] == '0' || b[x] == '1')
 {
