b8f1bed2884426deec2521b09dee229462616fd1
FouedDadi <foued.dadii@gmail.com>
2020-03-26 19:45:15 +0000

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,11 +1,13 @@
 #include "holberton.h"
 /**
- *print_biinary- function that print binary rep
+ *print_binary- function that print binary rep
  *@n: number
  */
 void print_binary(unsigned long int n)
 {
+unsigned long int x;
+x = (n & 1) + '0';
 if (n > 1)
 print_binary(n >> 1);
-_putchar((n & 1) + '0');
+_putchar(x);
 }
