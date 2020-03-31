10b2df868a7b64ed157757c2b5f5c16538f514e4
FouedDadi <foued.dadii@gmail.com>
2020-03-31 19:30:26 +0000

    all

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ *read_textfile- function that reads a text file and print it to POSIX
+ *@filename: filename
+ *@letters: number of letters
+ *Return: return 0 or w
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int o, r, w;
+char *file;
+file = malloc(sizeof(char) * letters);
+if (file == NULL)
+return (0);
+if (filename == NULL)
+return (0);
+o = open(filename, O_RDONLY);
+if (o == -1)
+return (0);
+r = read(o, file, letters);
+if (r == -1)
+return (0);
+w = write(STDOUT_FILENO, file, r);
+if (w == -1)
+return (0);
+close(o);
+free(file);
+return (w);
+}
