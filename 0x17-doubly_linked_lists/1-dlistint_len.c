c333be89be6524927e243b3a0a7223a12dae9b24
FouedDadi <foued.dadii@gmail.com>
2020-05-02 15:17:59 +0100

    len

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *print_dlistint- return number of elements of node
+ *@h: pointer to head of list
+ *Return: return x
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+int x;
+while (h != NULL)
+{
+h = h->next;
+x++;
+}
+return (x);
+}
