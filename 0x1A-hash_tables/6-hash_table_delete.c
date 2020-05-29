0b166179294098ecd582b930d8ba8a8c6241ebe1
FouedDadi <foued.dadii@gmail.com>
2020-05-29 23:29:52 +0100

    6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+*hash_table_delete- function that deletes a hash table
+*@ht: hash table
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int x;
+	hash_node_t *node;
+
+	if (ht == NULL)
+		return;
+	for (x = 0; x < ht->size; x++)
+	{
+		node = ht->array[x];
+		while (node)
+		{
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = node->next;
+		}
+	}
+}
