75c87d60a9577f940ddacda42074ec1f02cc1d8d
FouedDadi <foued.dadii@gmail.com>
2020-05-29 22:26:57 +0100

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,6 +10,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 hash_node_t *node, *head;
 unsigned long int index;
+if (!ht || !key || !value || strlen(key) == 0)
+		return (0);
 node = malloc(sizeof(hash_node_t));
 if (node == NULL)
 	return (0);
