a74f2498887f3a0ce8af8bc1fe5f9e8f04ca8681
FouedDadi <foued.dadii@gmail.com>
2020-05-29 22:38:32 +0100

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,39 +8,40 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-hash_node_t *node, *head;
-unsigned long int index;
-if (!ht || !key || !value || strlen(key) == 0)
+	hash_node_t *node, *head;
+	unsigned long int index;
+
+	if (!ht || !key || !value || !*key)
 		return (0);
-node = malloc(sizeof(hash_node_t));
-if (node == NULL)
-	return (0);
-index = key_index((const unsigned char *)key, ht->size);
-head = ht->array[index];
-while (head)
-{
-	if (strcmp(key, head->key) == 0)
+	index = key_index((const unsigned char *)key, ht->size);
+	head = ht->array[index];
+	while (head)
 	{
-		free(head->value);
-		head->value = strdup(value);
-		return (1);
+		if (strcmp(key, head->key) == 0)
+		{
+			free(head->value);
+			head->value = strdup(value);
+			return (1);
+		}
+		head = head->next;
 	}
-	head = head->next;
-}
-node->next = head;
-node->key = strdup(key);
-if (node->key == NULL)
-{
-	free(node);
-	return (0);
-}
-node->value = strdup(value);
-if (node->value == NULL)
-{
-	free(node);
-	free(node->key);
-	return (0);
-}
-head = node;
-return (1);
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (0);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node);
+		free(node->key);
+		return (0);
+	}
+	node->next = ht->array[index];
+	ht->array[index] = node;
+	return (1);
 }
