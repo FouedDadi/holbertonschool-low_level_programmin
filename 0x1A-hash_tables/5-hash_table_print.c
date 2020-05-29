b0433a60e90cdb85741f728a2d3e683eda30ebbe
FouedDadi <foued.dadii@gmail.com>
2020-05-29 23:16:55 +0100

    5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+*
+*
+*
+*
+*
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int x;
+	int p = 0;
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (x = 0; x < ht->size; x++)
+	{
+		node = ht->array[x];
+		while (node != NULL)
+		{
+			if (p)
+			printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			p++;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
