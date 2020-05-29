ddd4839bb2afe7cc8b9972e2f91b57e8e9b95da6
FouedDadi <foued.dadii@gmail.com>
2020-05-29 20:20:30 +0100

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,7 +7,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *table;
-unsigned int x;
+unsigned long int x;
 if (size == 0)
 return (NULL);
 table = malloc(sizeof(hash_table_t));
