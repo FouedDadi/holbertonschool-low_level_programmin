96bc573006307371cb4ec38162181ead1a8a2762
FouedDadi <foued.dadii@gmail.com>
2020-05-29 23:18:27 +0100

    5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,16 +1,14 @@
 #include "hash_tables.h"
 /**
-*
-*
-*
-*
-*
+*hash_table_print- function that print a hash table
+*@ht: hash table
 */
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *node;
 	unsigned long int x;
 	int p = 0;
+
 	if (ht == NULL)
 		return;
 	printf("{");
